b2f4e90b5d52095773a9e22127ad9f5b04043aaa
developer <prabesh065@gmail.com>
2023-08-19 14:00:34 +0545

    config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ... other config settings
+  optimizeDeps: {
+    include: [
+      'react', 'react-dom', // Add other dependencies here
+    ],
+  },
 })
