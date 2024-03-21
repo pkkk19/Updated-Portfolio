8f6d959290e1fbc0ff7ab9cca72a27cc23e14b96
developer <prabesh065@gmail.com>
2024-03-21 19:45:41 +0545

    Updates

---
+++ precache-manifest.b98856b11494f3c4d47a5848df6a227b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "003d6f10981159647a9a",
+    "url": "/pkkk19/static/css/main.1e6b03bf.chunk.css"
+  },
+  {
+    "revision": "003d6f10981159647a9a",
+    "url": "/pkkk19/static/js/main.003d6f10.chunk.js"
+  },
+  {
+    "revision": "66c154a22e2b7b2b9b37",
+    "url": "/pkkk19/static/js/1.66c154a2.chunk.js"
+  },
+  {
+    "revision": "4460b29b3c50ced4a4db",
+    "url": "/pkkk19/static/css/2.88d17d61.chunk.css"
+  },
+  {
+    "revision": "4460b29b3c50ced4a4db",
+    "url": "/pkkk19/static/js/2.4460b29b.chunk.js"
+  },
+  {
+    "revision": "8d7ad9fd74315e2b9efd",
+    "url": "/pkkk19/static/js/runtime~main.8d7ad9fd.js"
+  },
+  {
+    "revision": "e8a0d07265de7977ba192ce96e88a096",
+    "url": "/pkkk19/static/media/profile-img.e8a0d072.jpg"
+  },
+  {
+    "revision": "52df22006ad5f2b92d612ccf73b0613d",
+    "url": "/pkkk19/static/media/CV.52df2200.png"
+  },
+  {
+    "revision": "12e404483cc8635b25a3a8bb226975f5",
+    "url": "/pkkk19/static/media/Prabesh-Kumar-Shrestha-cv.12e40448.pdf"
+  },
+  {
+    "revision": "fd14f7e0fa2b7857451c825b2a8652f5",
+    "url": "/pkkk19/static/media/hero-bg.fd14f7e0.jpg"
+  },
+  {
+    "revision": "47e5194c77db90dfec2d0b2a1f15499f",
+    "url": "/pkkk19/index.html"
+  }
+];
