6d8be2cd737a20fde92940090fa509e46731a067
developer <prabesh065@gmail.com>
2024-04-08 19:11:20 +0545

    landing page

--- personalPortfolio/src/App.jsx
+++ personalPortfolio/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Sphere from "./assets/sphere";
 import Text from "./assets/Text";
+import Spline from "@splinetool/react-spline";
+import { Button } from "@chakra-ui/react";
 import "./App.css";
 
 import { Grid, GridItem } from "@chakra-ui/react";
@@ -13,24 +15,9 @@ export default class extends Component {
   };
   render() {
     return (
-      <div>
-        <div h="200%">
-          <Text />
-        </div>
-
-        <Grid
-          h="100%"
-          templateRows="repeat(2, 1fr)"
-          templateColumns="repeat(6, 1fr)"
-          gap={4}
-        >
-          <GridItem marginTop="15%" rowSpan={4} colSpan={1}>
-            <Sphere className="sphere" />
-          </GridItem>
-          <GridItem marginTop="90%" colSpan={2} rowSpan={2}></GridItem>
-        </Grid>
-        <Grid></Grid>
-      </div>
+      <>
+        <Spline scene="https://prod.spline.design/MubNeTEiYmgK9B3Z/scene.splinecode" />
+      </>
     );
   }
 }
--- personalPortfolio/src/assets/Text.jsx
+++ personalPortfolio/src/assets/Text.jsx
@@ -2,6 +2,6 @@ import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/Jgw-7dDXw0-FgLH7/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/MubNeTEiYmgK9B3Z/scene.splinecode" />
   );
 }
